f0e1818ced01a69cfd63942ac9b57fd33e66856f
AntonyJinny <gamer2007jin@gmail.com>
2024-01-19 23:50:33 -0500

    module config & some swerve module stuff!

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.utils.ModuleConfig;
+
+
+public class SwerveModule extends SubsystemBase {
+  /** Creates a new SwerveModule. */
+  public SwerveModule(ModuleConfig config) {
+
+    // initialize drive & turn motors
+    driveMotor = new CANSparkMax(config.DRIVE_PORT, MotorType.kBrushless);
+    turnMotor= new CANSparkMax(config.TURN_PORT, MotorType.kBrushless);
+
+    // get drive & turn encoders
+    driveEncoder = driveMotor.getEncoder();
+    turnEncoder = turnMotor.getAbsoluteEncoder(Type.kDutyCycle);
+
+    // converting the drive factors to meters and the turn factors to radians
+
+    // driveEncoder.setVelocityConversionFactor(((Math.PI * SwerveConstants.WHEEL_DIAMETER) / SwerveConstants.GEER_RATTIOLI) / 60); 
+    // driveEncoder.setPositionConversionFactor((Math.PI * SwerveConstants.WHEEL_DIAMETER) / SwerveConstants.GEER_RATTIOLI);
+
+    turnEncoder.setVelocityConversionFactor((Math.PI * 2) / 60);
+    turnEncoder.setPositionConversionFactor(Math.PI * 2);
+
+    this.config = config;
+  }
+
+  // Drive & turn motor
+  private final CANSparkMax driveMotor;
+  private final CANSparkMax turnMotor;
+
+  // Relative & Absolute Encoders
+  private final RelativeEncoder driveEncoder;
+  private final AbsoluteEncoder turnEncoder;
+
+//
+
+  public final ModuleConfig config;
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
