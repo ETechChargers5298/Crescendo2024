4dc201cb42941287772122fd91bdb8b06c43d235
Alexandra Chukhareva <alchukhareva@gmail.com>
2024-01-25 15:05:43 -0500

    I ADDED THE CONSTANTS VALUES - MS

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Intake extends SubsystemBase {
+    
+    private CANSparkMax intakeMotor;
+    private 
+
+}
