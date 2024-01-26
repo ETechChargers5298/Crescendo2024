4a07d6e191bce16c1a7e60f308ae803ec7f9ec16
USERNAME <EMAIL>
2024-01-25 20:23:06 -0500

    Ok

---
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Launcher extends SubsystemBase {
+private CANSparkFlex topMotor;
+private CANSparkFlex bottomMotor;
+private RelativeEncoder topEncoder;
+private RelativeEncoder bottomEncoder;
+
+private Launcher(){
+
+topMotor = new CANSparkFlex(Constants.MechConstants.TOP_LAUNCHER_MOTOR_PORT, MotorType.kBrushless);
+bottomMotor = new CANSparkFlex(Constants.MechConstants.BOTTOM_LAUNCHER_MOTOR_PORT, MotorType.kBrushless);
+
+topEncoder = topMotor.getEncoder();
+bottomEncoder = bottomMotor.getEncoder();
+}
+
+
+public void take(double speed){
+topMotor.set(-speed);
+bottomMotor.set(-speed);
+}
+
+
+public void launch(double speed){
+topMotor.set(speed);
+bottomMotor.set(speed);
+
+}
+public double getBottomEncoder(){
+double bottomPosition = bottomEncoder.getPosition();
+return bottomPosition;
+}
+
+public double getTopEncoder(){
+double topPosition = topEncoder.getPosition();
+return topPosition;
+
+}
+
+
+/*
+public void stop(){
+topMotor.set(0);
+bottomMotor.set(0);
+}
+*/
+
+}
