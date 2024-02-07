d128334cd8bc310a3e6fd2c1e930b66395a7f579
Krishnendu Barman <krishnendub@nycstudents.net>
2024-02-07 16:22:00 -0500

    added encoder to arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,16 +1,21 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase{
 
         private CANSparkMax motor;
+        private AbsoluteEncoder encoder;
         private static Arm instance;
 
         private Arm(){
             this.motor = new CANSparkMax(0, MotorType.kBrushless);
+            encoder = motor.getAbsoluteEncoder(Type.kDutyCycle);
         }
 
         public static Arm getInstance() {
@@ -19,6 +24,16 @@ public class Arm extends SubsystemBase{
             }
             return instance;
           }
+
+          public double getPosition(){
+            return encoder.getPosition();
+          }
+
+          public void resetValue(double offsetVal) {
+            encoder.setZeroOffset(offsetVal);
+          }
+            
+          
 
           public void pivot(double speed) {
             motor.set(speed);
