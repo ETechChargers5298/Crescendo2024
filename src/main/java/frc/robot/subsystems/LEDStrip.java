f5a77f96f37b3c4acf212f4efd5c9f64aef45056
AntonyJinny <gamer2007jin@gmail.com>
2024-02-20 17:44:09 -0800

    led stuffs

---
+++ src/main/java/frc/robot/subsystems/LEDStrip.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.utils.*;
+import frc.robot.utils.LedColors;
+
+/** Add your docs here. */
+public class LEDStrip extends SubsystemBase{
+
+    public static final double IntakeEat = LedColors.HOT_PINK;
+
+    public static final double HAVE_NOTE = LedColors.ORANGE; 
+
+    public static final double IN_GREEN_ZONE = LedColors.GREEN;
+
+    public static final double DISABLED = LedColors.BLUE_GREEN;
+    public static final double ENABLED = LedColors.BLUE_GREEN;
+
+    public static final double AMP_ANGLE = LedColors.YELLOW;
+    public static final double SPEAKER_ANGLE = LedColors.VIOLET;
+    public static final double CLIMBER_REACHED_MAX = LedColors.WHITE;
+
+
+
+
+
+
+    
+}
+
