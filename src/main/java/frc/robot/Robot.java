a3ac4aa3aeeff27fbed1efeb442246c0ba0aec30
vanessa0135 <vanessab83@nycstudents.net>
2024-02-26 23:16:16 -0500

    made shooter intake changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,11 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.MechConstants;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.LEDStrip;
 import frc.robot.subsystems.LEDStrip.SubsystemsPriority;
+import frc.robot.subsystems.Launcher;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -48,6 +51,12 @@ private Command m_autonomousCommand;
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    
+    Launcher.getInstance().launch(0.6);
+    
+    if(Intake.getInstance().checkNoteFound()) {
+      Launcher.getInstance().launch(MechConstants.LAUNCHER_SPEED);
+    }
   }
 
   public static boolean isBlue() {
